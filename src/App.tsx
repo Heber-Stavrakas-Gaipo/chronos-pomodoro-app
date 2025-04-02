c110628fd6772ed3fa3168f39f9ef39df9821d9c
Heber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2025-04-02 12:34:14 -0300

    :construction: create form

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,26 @@ function App() {
           <CountDown />
         </Content>
       </Container>
+      <Container>
+        <Content>
+          <form className='form' action=''>
+            <div className='formRow'>
+              <label htmlFor='input'>Tarefa:</label>
+              <input id='input' type='text' />
+            </div>
+            <div className='formRow'>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className='formRow'>
+              <p>Ciclos</p>
+              <p>0 0 0 0 0</p>
+            </div>
+            <div className='formRow'>
+              <button>Enviar</button>
+            </div>
+          </form>
+        </Content>
+      </Container>
     </>
   );
 }
